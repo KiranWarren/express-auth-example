a5afba4654053fd602421f37a24b034a646077d8
Kiran Warren <kiran.m.warren@gmail.com>
2024-01-23 18:58:54 +1000

    created scaffolding for user routes

---
+++ src/controllers/UserController.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+const router = express.Router();
+
+// ROUTES
+// Get all users from database
+// localhost:3000/users/
+/**
+ * [
+ * {
+ *      userID:
+ *      username:
+ * },
+ * {
+ *      userID:
+ *      username:
+ * },
+ * {
+ *      userID:
+ *      username:
+ * }
+ * ]
+ */
+router.get("/", async (request, response) => {
+  return null;
+});
+
+// Get a user by ID
+// localhost:3000/users/234313412413
+/**
+ * {
+ *      userID:
+ *      username:
+ * }
+ */
+router.get("/:user_id", async (request, response) => {
+  return null;
+});
+
+// Create a new user in the database
+// localhost:3000/users/register
+// request.body = {username: "admin", password: "password1"}
+// respond with {jwt: 7y743hfh9fhwr8hf98fh4}
+router.post("/register", async (request, response) => {
+  return null;
+});
+
+// Log in a user
+// localhost:3000/users/login
+// request.body = {username: "admin", password: "password1"}
+// respond with {jwt: 7y743hfh9fhwr8hf98fh4}
+router.post("/login", async (request, response) => {
+  return null;
+});
+
+// Verify user using their jwt
+// localhost:3000/users/verify
+// JWT in request.headers["jwt"] or request.header["authorization"]
+// respond with {jwt: 7y743hfh9fhwr8hf98fh4}
+router.get("/verify", async (request, response) => {
+  return null;
+});
+
+// Regenerate jwt when jwt expires
+// localhost:3000/users/regen
+// JWT in request.headers["jwt"] or request.header["authorization"]
+// respond with {jwt: 7y743hfh9fhwr8hf98fh4}
+router.get("/regen", async (request, response) => {
+  return null;
+});
