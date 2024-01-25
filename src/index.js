a57595886df65773da514caeb1fd52528d78b5c3
Kiran Warren <kiran.m.warren@gmail.com>
2024-01-25 18:48:36 +1000

    changed port to variable from env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const { databaseConnect } = require("./database");
 const { app } = require("./server");
 
-app.listen(3000, async () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, async () => {
   await databaseConnect();
   console.log("Server is running...");
 });
