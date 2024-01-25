ce3107a4d0350f67570010ed4f51c166a99bdafc
Kiran Warren <kiran.m.warren@gmail.com>
2024-01-25 19:45:57 +1000

    finished thurs lesson

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+var corsOptions = {
+  origin: ["localhost:3000/"],
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (request, response) => {
   response.json({
